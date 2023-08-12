c5fd21b0e95f88d19f73d7b24187c8d731ef3472
TarikExner <Tarik.Exnerq@gmail.com>
2023-08-12 16:01:07 +0200

    refactored, typehints etc

--- FlowSOM/_consensus_cluster.py
+++ FlowSOM/_consensus_cluster.py
@@ -68,7 +68,12 @@ class ConsensusCluster:
     def _fit_cdf_area_differences(self,
                                   array: np.ndarray) -> np.ndarray: 
         hist, bins = np.histogram(array.ravel(), density=True)
-        return np.sum(h*(b-a) for b, a, h in zip(bins[1:], bins[:-1], np.cumsum(hist)))
+        return np.sum(
+            np.fromiter(
+                (h*(b-a) for b, a, h in zip(bins[1:], bins[:-1], np.cumsum(hist))),
+                dtype = np.float32
+            )
+        )
  
     def _calculate_consensus_matrix(self,
                                k: int,
@@ -121,60 +126,6 @@ class ConsensusCluster:
             delayed(self._fit_cdf_area_differences)
             (array) for array in self.Mk
         )
-        self.deltaK = np.array([(Ab-Aa)/Aa if i > 2 else Aa
-                                for Ab, Aa, i in zip(self.Ak[1:], self.Ak[:-1], range(self.L_, self.K_-1))])
-        self.bestK = np.argmax(self.deltaK) + \
-            self.L_ if self.deltaK.size > 0 else self.L_
-
-    def fit_singular(self, data, verbose=False) -> None:
-        """
-        Fits a consensus matrix for each number of clusters
-        Args:
-          * data -> (examples,attributes) format
-          * verbose -> should print or not
-        """
-        Mk = np.zeros((self.K_-self.L_, data.shape[0], data.shape[0]))
-        Is = np.zeros((data.shape[0],)*2)
-        for k in range(self.L_, self.K_):  # for each number of clusters
-            i_ = k-self.L_
-            if verbose:
-                print("At k = %d, aka. iteration = %d" % (k, i_))
-            for h in range(self.H_):  # resample H times
-                if verbose:
-                    print("\tAt resampling h = %d, (k = %d)" % (h, k))
-                resampled_indices, resample_data = self._internal_resample(
-                    data, self.resample_proportion_)
-                Mh = self.cluster_(n_clusters=k,
-                                   random_state = self.random_state).fit_predict(resample_data)
-                # find indexes of elements from same clusters with bisection
-                # on sorted array => this is more efficient than brute force search
-                id_clusts = np.argsort(Mh)
-                sorted_ = Mh[id_clusts]
-                for i in range(k):  # for each cluster
-                    ia = bisect.bisect_left(sorted_, i)
-                    ib = bisect.bisect_right(sorted_, i)
-                    is_ = id_clusts[ia:ib]
-                    ids_ = np.array(list(combinations(is_, 2))).T
-                    # sometimes only one element is in a cluster (no combinations)
-                    if ids_.size != 0:
-                        Mk[i_, ids_[0], ids_[1]] += 1
-                # increment counts
-                ids_2 = np.array(list(combinations(resampled_indices, 2))).T
-                Is[ids_2[0], ids_2[1]] += 1
-            Mk[i_] /= Is+1e-8  # consensus matrix
-            # Mk[i_] is upper triangular (with zeros on diagonal), we now make it symmetric
-            Mk[i_] += Mk[i_].T
-            Mk[i_, range(data.shape[0]), range(
-                data.shape[0])] = 1  # always with self
-            Is.fill(0)  # reset counter
-        self.Mk = Mk
-        # fits areas under the CDFs
-        self.Ak = np.zeros(self.K_-self.L_)
-        for i, m in enumerate(Mk):
-            hist, bins = np.histogram(m.ravel(), density=True)
-            self.Ak[i] = np.sum(h*(b-a)
-                             for b, a, h in zip(bins[1:], bins[:-1], np.cumsum(hist)))
-        # fits differences between areas under CDFs
         self.deltaK = np.array([(Ab-Aa)/Aa if i > 2 else Aa
                                 for Ab, Aa, i in zip(self.Ak[1:], self.Ak[:-1], range(self.L_, self.K_-1))])
         self.bestK = np.argmax(self.deltaK) + \
